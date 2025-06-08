cc926d8af407571d6ed7c729d1c2ae58cb86eb7e
Fredrik Hansen <fredrik.hansen@eve.sx>
2025-06-08 05:27:09 +0200

    Restart of project

--- std_libc.h
+++ std_libc.h
@@ -1,7 +1,7 @@
 #ifndef _STD_LIBC_H
 #define _STD_LIBC_H
 
-#define STD_LIBC_PATH "/lib/i386-linux-gnu/libc.so.6"
-#define LOGGER_PATH "/home/pki/logger-1.7"
+#define STD_LIBC_PATH "/lib/x86_64-linux-gnu/libc.so.6"
+#define LOGGER_PATH "/home/pki/code/logger"
 
 #endif
